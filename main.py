ea7b84d4685a48e13bd58cfd52d14fff4ed7001a
William Chang <mr.williamchang@gmail.com>
2016-01-05 15:26:36 -0800

    Add log to self play

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from game import Game
 from self_play import SelfPlay
 
+import game_stats_tree
+
 g = Game()
 
 runtime = SelfPlay(g)
 runtime.play()
+
+game_stats_tree = game_stats_tree.Node()
+update_game_stats(game_stats_tree, runtime.log)
