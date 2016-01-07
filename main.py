6d695592af0c5f9a2fe5a3fd2d7399a5cf9ca184
William Chang <mr.williamchang@gmail.com>
2016-01-07 11:53:41 -0800

    Add ability to train models from command line

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import game_stats_tree
 
 # Setup command line arguments
 parser = argparse.ArgumentParser(description="Train and play against a Connect 4 AI.")
-command_parser = parser.add_subparsers()
+command_parser = parser.add_subparsers(dest="command")
 
 # Configure train command
 train_parser = command_parser.add_parser("train", help="Train models")
@@ -30,38 +30,46 @@ compare_parser.add_argument("--iters", nargs=1, type=int, default=100,
 play_parser = command_parser.add_parser("play", help="Play against a model")
 play_parser.add_argument("model", nargs=1, help="Models to play against")
 
-args = parser.parse_args()
-print(args)
 
-# pickle_filename = "game_stats.pickle"
-# if os.path.isfile(pickle_filename):
-#   with open(pickle_filename, "rb") as pickle_file:
-#     game_stats = pickle.load(pickle_file)
-# else:
-#   game_stats = game_stats_tree.Node()
 
-# move_chooser = BestKnownMoveChooser(game_stats, exploitation_rate=0.7)
-# game_stats_tree.print_stats(game_stats)
+def train(models, num_iters):
+  for model_path in models:
+    if os.path.isfile(model_path):
+      with open(model_path, "rb") as pickle_file:
+        game_stats = pickle.load(pickle_file)
+    else:
+      game_stats = game_stats_tree.Node()
 
-# runtime = SelfPlay(move_chooser)
+    move_chooser = BestKnownMoveChooser(game_stats, exploitation_rate=0.7)
+    game_stats_tree.print_stats(game_stats)
 
-# wins = 0
-# num_rounds = 0#1000000
-# for i in range(num_rounds):
-#   g = Game()
-#   runtime.play(g)
-#   game_stats_tree.update_game_stats(game_stats, runtime.log, g.winner)
-#   if g.winner is DiscState.red:
-#     wins += 1
-#   if i % 500 == 0 and i > 0:
-#     print("Runs: %i 1st player win %%: %f" % (i, wins / i))
-#   if i % 100000 == 0 and i > 0:
-#     with open(pickle_filename, "wb") as pickle_file:
-#       pickle.dump(game_stats, pickle_file)
-# print("Runs: %i" % num_rounds)
+    runtime = SelfPlay(move_chooser)
 
-# with open(pickle_filename, "wb") as pickle_file:
-#   pickle.dump(game_stats, pickle_file)
+    wins = 0
+    for i in range(num_iters):
+      g = Game()
+      runtime.play(g)
+      game_stats_tree.update_game_stats(game_stats, runtime.log, g.winner)
+      if g.winner is DiscState.red:
+        wins += 1
+      if i % 500 == 0 and i > 0:
+        print("Runs: %i 1st player win %%: %f" % (i, wins / i))
+      if i % 100000 == 0 and i > 0:
+        with open(model_path, "wb") as pickle_file:
+          pickle.dump(game_stats, pickle_file)
+    print("Runs: %i" % num_iters)
+
+    with open(model_path, "wb") as pickle_file:
+      pickle.dump(game_stats, pickle_file)
+
+def main():
+  args = parser.parse_args()
+  if args.command == "train":
+    train(args.models, args.iters)
+
+
+
+
 
 # PLAY = True
 # if PLAY:
@@ -69,3 +77,6 @@ print(args)
 #   runtime = TextualRuntime(g, BestKnownMoveChooser(game_stats, exploitation_rate=1.0,
 #     verbose=True))
 #   runtime.start()
+
+if __name__ == "__main__":
+  main()
