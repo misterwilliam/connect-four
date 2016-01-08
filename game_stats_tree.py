6bdbc2b66df762edb397263bfeded6a5741f850e
William Chang <mr.williamchang@gmail.com>
2016-01-08 12:01:26 -0800

    Add test for BestKnownMoveChooser

--- game_stats_tree.py
+++ game_stats_tree.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 from game import DiscState
 
 class Node:
---
+++ test_best_known_move_chooser.py
@@ -0,0 +1,54 @@
+import unittest
+
+from game import DiscState
+from grid import Grid
+from game_stats_tree import Node
+
+from best_known_move_chooser import *
+
+
+class BestKnownMoveChooserTests(unittest.TestCase):
+
+  def test_simple_playthrough(self):
+    grid = Grid(2, 2, initial_value=DiscState.empty)
+    root = Node(children={
+                  0: Node(children={
+                            0: Node(data={
+                                      DiscState.red: 1,
+                                      DiscState.black: 3,
+                                    }),
+                            1: Node(data={
+                                      DiscState.red: 5,
+                                      DiscState.black: 0
+                                    }),
+                          },
+                          data={
+                            DiscState.red: 6,
+                            DiscState.black: 3
+                          }),
+                  1: Node(children={
+                            0: Node(data={
+                                DiscState.red: 2,
+                                DiscState.black: 8
+                              }),
+                            1: Node(data={
+                                DiscState.red: 3,
+                                DiscState.black: 3,
+                              })
+                          },
+                          data={
+                            DiscState.red: 5,
+                            DiscState.black: 11
+                          })
+                })
+    move_chooser = BestKnownMoveChooser(root)
+
+    self.assertEqual(0, move_chooser.request_move(DiscState.red, grid, [0, 1]))
+    move_chooser.report_move(0)
+    self.assertEqual(1, move_chooser.request_move(DiscState.red, grid, [0, 1]))
+
+    move_chooser.restart()
+
+    self.assertEqual(0, move_chooser.request_move(DiscState.red, grid, [0, 1]))
+    move_chooser.report_move(0)
+    self.assertEqual(1, move_chooser.request_move(DiscState.red, grid, [0, 1]))
